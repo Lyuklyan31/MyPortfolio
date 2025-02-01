7a2f4b87f2a74647a3d68ebd036d08acf43c395c
Oleh <lyuclyan.oleg14@gmail.com>
2025-02-01 13:51:42 +0200

    Created SwiftCharts.

---
+++ SwiftCharts/SwiftCharts/MainContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct MainContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ SwiftCharts/SwiftCharts/SwiftChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsApp.swift
+//  SwiftCharts
+//
+//  Created by Oleh Liuklian  on 01.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
